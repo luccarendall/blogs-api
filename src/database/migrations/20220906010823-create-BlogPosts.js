76141cfb169ff71b34820c25a05516d2c536a3ea
Lucca Rendall <luccarendall2@gmail.com>
2022-09-05 22:12:34 -0300

    Migration da tabela BlogPosts criada

--- src/database/migrations/20220906010823-create-BlogPosts.js
+++ src/database/migrations/20220906010823-create-BlogPosts.js
@@ -2,20 +2,42 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
-};
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        },
+        published: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated: {
+          type: Sequelize.DATE,
+         allowNull: false,
+        },
+      })
+    },
+  
+    down: async (queryInterface, Sequelize) => {
+       await queryInterface.dropTable('BlogPosts');
+  
+    }
+  };
