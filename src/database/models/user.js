5f9693155afe03d0e137b1a1c5d69260a4116745
Lucca Rendall <luccarendall2@gmail.com>
2022-09-05 22:34:25 -0300

    fix: Model User

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -4,11 +4,17 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: {
       type: DataTypes.INTEGER,
-      primaryKey: true
-    } ,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    
     displayName: DataTypes.STRING,
+    
     email: DataTypes.STRING,
+    
     password: DataTypes.STRING,
+    
     image: DataTypes.STRING,
   }, {
     timestamps: false,
